85fd468a3f313c5e64598fc0508b8537ea07442f
Fuentes-Elmer <delacruzelmer27@gmail.com>
2025-04-19 20:04:50 -0600

    Creaciòn de clase conexion

---
+++ Datos/Conexion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        #region = "P.NO. 1 Creación conexión y validación de Status";
+        //cadena de conexión
+        private SqlConnection db_conexion = new SqlConnection("Data Source=Fuentes-Elmer;Initial Catalog=Sistema_Hotel;Integrated Security=True;Encrypt=False");
+
+        public SqlConnection MtdAbrirConexion() //abrir conexión
+        {
+            if (db_conexion.State == ConnectionState.Closed) //Verifica si la conexión es cerrada
+                db_conexion.Open(); //devuelve la conexión abierta
+            return db_conexion; //retornar conexión OK
+        }
+
+        public SqlConnection MtdCerrarConexion()
+        {
+            if (db_conexion.State == ConnectionState.Open) //verifica si la conezión es abierta
+                db_conexion.Close(); // cierra la conexión
+            return db_conexion; //retorna la conexión cerrada
+        }
+        #endregion;
+
+    }
+}
